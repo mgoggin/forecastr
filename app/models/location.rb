a418674ca803c834d5d87c90d53e777331c6f4ff
Mike Goggin <michael@mgoggin.com>
2024-04-10 16:41:03 -0500

    chore(geocoding): Clean up `Location`

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,11 +1,13 @@
 # A location on Earth.
-Location = Data.define(:latitude, :longitude, :postal_code) do
-  # Coordinates of the location.
-  #
-  # @return String|nil latitude and longitude, if present, separated by a comma
-  def coordinates
-    raise ":latitude and :longitude must both be provided" if latitude.blank? || longitude.blank?
-
-    [latitude, longitude].compact.presence&.join(", ")
-  end
-end
+#
+# This is a value object that holds the latitude, longitude, and postal code for a specific location. The values for
+# these attributes will come from whatever geocoding service is being used.
+#
+# @example
+#   location = Location.new(latitude: 123.00000, longitude: -80.98765, postal_code: "12345")
+#
+# @param latitude [String|Numeric]
+# @param longitude [String|Numeric]
+# @param postal_code [String]
+# @return self
+Location = Data.define(:latitude, :longitude, :postal_code)
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,6 +1,4 @@
-require "active_support/all"
-require "spec_helper"
-require File.expand_path("../../app/models/location", __dir__)
+require "rails_helper"
 
 RSpec.describe Location do
   subject(:instance) { described_class.new(latitude:, longitude:, postal_code:) }
@@ -10,35 +8,4 @@ RSpec.describe Location do
   let(:postal_code) { nil }
 
   it { is_expected.to have_attributes(latitude:, longitude:, postal_code:) }
-
-  describe "#coordinates" do
-    subject(:result) { instance.coordinates }
-
-    it "is expected to raise RuntimeError" do
-      expect { result }.to raise_error ":latitude and :longitude must both be provided"
-    end
-
-    context "when latitude and longitude are both provided" do
-      let(:latitude) { 1 }
-      let(:longitude) { 2 }
-
-      it { is_expected.to eq "1, 2" }
-    end
-
-    context "when latitude only is provided" do
-      let(:latitude) { 1 }
-
-      it "is expected to raise RuntimeError" do
-        expect { result }.to raise_error ":latitude and :longitude must both be provided"
-      end
-    end
-
-    context "when longitude only is provided" do
-      let(:longitude) { 2 }
-
-      it "is expected to raise RuntimeError" do
-        expect { result }.to raise_error ":latitude and :longitude must both be provided"
-      end
-    end
-  end
 end
